3b94c6eaae0627260fbf4f8ec41c9cee513d0ec3
Salazar1022 <ssalazarh3@eafit.edu.co>
2025-09-21 19:38:56 -0500

    server creation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Starting server...");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json('Bienvenido al servidor de MAGNETO Embeddable Tool');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
